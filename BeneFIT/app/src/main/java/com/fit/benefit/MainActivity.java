9647b82acae4467ec54caa274dd2b35c82da6615
Gloria0401 <82899043+Gloria0401@users.noreply.github.com>
2021-05-11 17:19:49 +0200

    Add files via upload
    Seconda commit.
    Splash screen, main e login collegate tra loro

--- BeneFIT/app/src/main/java/com/fit/benefit/MainActivity.java
+++ BeneFIT/app/src/main/java/com/fit/benefit/MainActivity.java
@@ -2,13 +2,34 @@ package com.fit.benefit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.fit.benefit.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    public void login() {
+        Intent intent = new Intent (this, LoginActivity.class);
+        startActivity(intent);
     }
 }
