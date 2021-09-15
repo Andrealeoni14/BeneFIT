138767b2c2d3f72c8da69f03f82dbe4c62adf633
Alessia0401 <82898863+Alessia0401@users.noreply.github.com>
2021-09-15 11:35:32 +0200

    Add files via upload
    Eliminazione classi inutilizzate e aggiustamenti

---
+++ BeneFIT/app/src/main/java/com/fit/benefit/WorkoutActivity.java
@@ -0,0 +1,185 @@
+// activity for viewing the workout details
+package com.fit.benefit;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
+import com.squareup.picasso.Picasso;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseUser;
+
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.fit.benefit.ExerciseActivity.EXTRA_CAT;
+import static com.fit.benefit.ExerciseActivity.EXTRA_DESC;
+import static com.fit.benefit.ExerciseActivity.EXTRA_IMAGE;
+import static com.fit.benefit.ExerciseActivity.EXTRA_INDEX;
+import static com.fit.benefit.ExerciseActivity.EXTRA_NAME;
+import static com.fit.benefit.ExerciseActivity.EXTRA_RETR;
+import static com.fit.benefit.LoginActivity.fCat;
+import static com.fit.benefit.LoginActivity.favorites;
+import static com.fit.benefit.LoginActivity.findCatIndex;
+
+public class WorkoutActivity extends AppCompatActivity {
+
+    private int category;
+    private int index;
+
+    private int i = 0;
+    private boolean favorite = false;
+    private int catIndex;
+    private Button favoriteButton;
+    private Bundle extras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        extras = intent.getExtras();
+
+        String imageUrl = extras.getString(EXTRA_IMAGE);
+        String name = extras.getString(EXTRA_NAME);
+        String description = extras.getString(EXTRA_DESC);
+        category = extras.getInt(EXTRA_CAT);
+        index = extras.getInt(EXTRA_INDEX);
+
+
+        ImageView im = findViewById(R.id.exe_image);
+        TextView textName = findViewById(R.id.exe_name);
+        TextView textDescription = findViewById(R.id.exe_description);
+
+        // checks if the workout is already in favourites
+        catIndex = category - fCat;
+        while((i < favorites[catIndex].size())&&(!favorite)) {
+            if(favorites[catIndex].get(i)==index) {
+                favorite=true;
+            }
+            else {
+                i++;
+            }
+        }
+        favoriteButton = findViewById(R.id.button);
+        if(favorite) {
+            favoriteButton.setText(R.string.remSave);
+        }
+
+        textName.setText(name);
+        textDescription.setText(description);
+        if(imageUrl != null) {
+            Picasso.with(this).load(imageUrl).fit().centerInside().into(im);
+        }
+        else {
+            Picasso.with(this).load(imageUrl).fit().placeholder(R.drawable.img_default).into(im);
+        }
+    }
+
+
+    // adds or remove the workout to the saved ones on firestore
+    public void favorite(android.view.View View) {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        Map<String, Object> update = new HashMap<>();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        CollectionReference collRef = database.collection(user.getUid());
+        DocumentReference currentCat = collRef.document(Integer.toString(category));
+        if (!favorite) {
+            favorites[catIndex].add(index);
+            update.put(Integer.toString(index), index);
+            currentCat.set(update, SetOptions.merge());
+            collRef.get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                favorite = true;
+                                changeSize(collRef, task, true);
+                                favoriteButton.setText(R.string.remSave);
+                            }
+                        }
+                    });
+
+        }
+        else {
+            favorites[catIndex].remove(i);
+            update.put(Integer.toString(index), FieldValue.delete());
+            currentCat.update(update).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull @NotNull Task<Void> task) {
+                    favorite = false;
+                    favoriteButton.setText(R.string.save);
+                }
+            });
+            collRef.get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                changeSize(collRef, task, false);
+                            }
+                        }
+                    });
+        }
+    }
+
+
+    // modify the size field on firestore (the variable counts how many saved workouts there are)
+    public void changeSize(CollectionReference collRef, Task<QuerySnapshot> task, boolean add) {
+        QuerySnapshot collection = task.getResult();
+        List<DocumentSnapshot> categories = collection.getDocuments();
+        int catIndex = findCatIndex(categories, category - fCat);
+        long size = (long) categories.get(catIndex).get("Size");
+        if(add) {
+            size++;
+        }
+        else {
+            size--;
+        }
+        Map<String, Integer> nSize = new HashMap<>();
+        nSize.put("Size", (int) size);
+        collRef.document(Integer.toString(category))
+                .set(nSize, SetOptions.merge());
+    }
+
+
+    public void logoutClick(android.view.View view) {
+        LogoutActivity logout = new LogoutActivity();
+        logout.logoutFunction(WorkoutActivity.this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        int iCat = extras.getInt(EXTRA_RETR);
+
+        Intent intent = new Intent (this, ExerciseActivity.class);
+        intent.putExtra("category", iCat);
+        startActivity(intent);
+
+    }
+}
---
+++ BeneFIT/app/src/main/java/com/fit/benefit/utils/Constants.java
@@ -0,0 +1,20 @@
+package com.fit.benefit.utils;
+
+public class Constants {
+
+    public static final String EXERCISES_API_BASE_URL = "https://wger.de/api/v2/exercise/?language=2&limit=300&offset=0";
+    public static final String EXERCISES_API_CATEGORY_URL = "https://wger.de/api/v2/exercise/?language=2&limit=300&offset=0&category=";
+    public static final String  EXERCISES_IMAGE_API_URL = "https://wger.de/api/v2/exerciseimage/?is_main=true&exercise_base=";
+    
+    public static final String  EXERCISES_API_KEY = "AIzaSyCXhEsEkCXOEgxwSL-ZpajbWLA_PGEAeMo";
+
+    public static final int FRESH_TIMEOUT = 3600*1000*5; // 5 hour in milliseconds
+    public static final String LAST_UPDATE = "lastUpdate";
+
+    public static final String DATABASE_NAME = "exercise_database.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String FIREBASE_DB = "";
+    public static final String USER = "users";
+    public static final String FAVORITE_EXERCISE = "favorite_Exercise";
+}
